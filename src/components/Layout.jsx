2bdbbc2e94c07829b86d36b5350630ccb5b0be09
Roman Kezik <roma2003kezik@gmail.com>
2026-09-08 08:42:53 +0300

    chore: remove unused import and unused parameter

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -13,7 +13,7 @@ function Layout() {
 
           <span className="user-name">{user.name}</span>
           <button className=' nav-link'
-            onClick={(e) => { logout(); }}>Выйти</button>
+            onClick={() => { logout(); }}>Выйти</button>
         </div>
       </nav>
       <main>
